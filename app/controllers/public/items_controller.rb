8d434a9f115cb6e9bf104c7a66f69938e4171dd5
EC2 Default User <ec2-user@ip-172-31-84-108.ec2.internal>
2021-10-02 15:05:34 +0000

    [Add] 商品詳細途中まで

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,10 @@
 class Public::ItemsController < ApplicationController
-  #public/商品一覧・詳細
   def show
     @item = Item.find(params[:id])
-
+    
   end
 
   def index
-    @items = Item.all.page(params[:page]).per(8)
+    @items = Item.where(is_active: true).page(params[:page]).per(8)
   end
-
-
 end
