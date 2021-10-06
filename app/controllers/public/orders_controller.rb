b043697c2cd68744a058d77cac9279be4da82f06
EC2 Default User <ec2-user@ip-172-31-84-108.ec2.internal>
2021-10-06 14:31:21 +0000

    [Add]注文情報確認画面まで

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -7,17 +7,30 @@ class Public::OrdersController < ApplicationController
 
   def confirm
     @order = Order.new(order_params)
-    if
-    @address = Address.find(params[:order][:address_id])
-    @order.postal_code = @address.postal_code
-    @order.address = @address.address
-    @order.name = @address.name
+    @cart_items = current_customer.cart_items
+    @sum = 0#初期値として必要
+    @shipping_cost = 800#今回は固定で800円なので
+
+    if params[:order][:select_address] == "0"#自分の住所
+       @order.postal_code = current_customer.postal_code
+       @order.address = current_customer.address
+       @order.name = current_customer.first_name + current_customer.last_name
+
+    elsif params[:order][:select_address] == "1"#登録済み住所
+       @address = Address.find(params[:order][:address_id])
+       @order.postal_code = @address.postal_code
+       @order.address = @address.address
+       @order.name = @address.name
+
+    elsif params[:order][:select_address] == "2"#新しいお届け先
+       @order.postal_code = params[:order][:postal_code]
+       @order.address = params[:order][:address]
+       @order.name = params[:order][:name]
+    end
+  end
 
 
   def create#注文確定（注文情報入力画面で確定するボタンを押してデータ保存）
-    # @order = Order.new(order_params)
-    # @order.save
-    # redirect_to orders_confirm_path(@order.id)
   end
 
   def complete
